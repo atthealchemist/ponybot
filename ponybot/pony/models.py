e94e1f2d86a38db96621e30c395511a175475785
The <at.thealchemist@gmail.com>
2021-07-05 16:46:54 +0300

    Added id field to Pony model, added feed() method to Pony model

--- ponybot/pony/models.py
+++ ponybot/pony/models.py
@@ -1,3 +1,5 @@
+import uuid
+
 from django.db import models
 from django.utils.translation import gettext as _
 from django.core.validators import MinValueValidator, MaxValueValidator
@@ -6,6 +8,7 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 
 
 class Pony(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(_("Pony name"), max_length=64, blank=True)
     experience = models.PositiveSmallIntegerField(
         _("Pony experience"),
@@ -19,3 +22,9 @@ class Pony(models.Model):
         _("Pony satiety"),
         default=10
     )
+
+    def feed(self):
+        if self.satiety >= self.experience * 14:
+            return
+        self.satiety += 1
+        self.save(update_fields=['satiety'])
