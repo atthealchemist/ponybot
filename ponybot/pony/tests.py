d3796f760b0f3bc290a39f0c973f29fe82b00625
The <at.thealchemist@gmail.com>
2021-07-06 09:06:04 +0300

    Rewriting tests using pytest style

--- ponybot/pony/tests.py
+++ ponybot/pony/tests.py
@@ -1,75 +1,89 @@
 from datetime import datetime
 import time
+import pytest
 
 from django.test import TestCase, TransactionTestCase
 from .models import Pony
 
+
 # Create your tests here.
 
 
-class PonyTestCase(TransactionTestCase):
+@pytest.mark.django_db
+def test_pony_created():
+    my_pony = Pony.objects.create(
+        name="Applejack",
+        satiety=16
+    )
+    assert my_pony is not None
+    assert my_pony.name == "Applejack"
 
-    def test_pony_created(self):
-        my_pony = Pony(
-            name="Lauren Faust"
-        )
 
-        self.assertIsNotNone(my_pony)
-        self.assertEqual("Lauren Faust", my_pony.name)
+@pytest.mark.django_db
+def test_pony_has_experience():
+    my_pony = Pony.objects.create(
+        name="Applejack",
+        satiety=16
+    )
+    assert my_pony.experience is not None
+    assert 0 < my_pony.experience <= 10
 
-        pinkie = Pony(
-            name="Pinkie Pie"
-        )
-        self.assertNotEqual(my_pony.name, pinkie.name)
 
-    def test_pony_has_default_experience(self):
-        my_pony = Pony(
-            name="Lauren"
-        )
-        self.assertIsNotNone(my_pony.experience, 1)
-        self.assertGreater(my_pony.experience, 0)
-        self.assertLess(my_pony.experience, 10)
+@pytest.mark.django_db
+def test_pony_has_satiety():
+    my_pony = Pony.objects.create(
+        name="Applejack",
+        satiety=12
+    )
+    assert my_pony.satiety is not None
+    assert 0 < my_pony.satiety <= my_pony.experience + 14
 
-    def test_pony_has_default_satiety(self):
-        my_pony = Pony(name="Rarity")
-        self.assertIsNotNone(my_pony.satiety)
-        self.assertGreater(my_pony.satiety, 1)
-        self.assertLess(my_pony.satiety, my_pony.experience + 14)
 
-    def test_pony_feed(self):
-        my_pony = Pony(name="Fluttershy")
-        satiety_before = my_pony.satiety
-        my_pony.feed()
-        self.assertGreater(my_pony.satiety, satiety_before)
+@pytest.mark.django_db
+def test_pony_feeding():
+    my_pony = Pony.objects.create(
+        name="Twilight Sparkle",
+        satiety=12
+    )
+    satiety_before = my_pony.satiety
+    my_pony.feed()
+    assert my_pony.satiety > satiety_before
 
-        aj_pony = Pony(
-            name="Applejack",
-            satiety=14
-        )
-        satiety_before = aj_pony.satiety
-        aj_pony.feed()
-        self.assertEqual(aj_pony.satiety, satiety_before)
+    aj_pony = Pony.objects.create(name="Applejack", satiety=14)
+    satiety_before = aj_pony.satiety
+    aj_pony.feed()
+    assert aj_pony.satiety == satiety_before
 
-    def test_pony_last_feeded(self):
-        my_pony = Pony(
-            name="Rainbow Dash"
-        )
 
-        my_pony.feed()
+@pytest.mark.django_db
+def test_pony_last_feeded():
+    my_pony = Pony.objects.create(
+        name="Rainbow Dash"
+    )
 
-        self.assertEqual(
-            (my_pony.last_feeding.hour, my_pony.last_feeding.minute),
-            (datetime.now().hour, datetime.now().minute)
-        )
+    my_pony.feed()
 
-    def test_pony_first_feed(self):
-        my_pony = Pony(name="Sweetie Belle")
-        my_pony.feed()
+    my_pony_feeding_hm = (
+        my_pony.last_feeding.hour, my_pony.last_feeding.minute
+    )
+    now_hm = (
+        datetime.now().hour, datetime.now().minute
+    )
 
-        my_pony.feed()
-        self.assertNotEqual(my_pony.first_feeding, my_pony.last_feeding)
+    assert my_pony_feeding_hm == now_hm
 
-    def test_pony_stats(self):
-        my_pony = Pony(name="Derpy Hooves")
 
-        self.assertIn('Derpy', str(my_pony))
+@pytest.mark.django_db
+def test_pony_first_feed():
+    my_pony = Pony.objects.create(name="Sweetie Belle")
+    my_pony.feed()
+
+    my_pony.feed()
+    assert my_pony.first_feeding != my_pony.last_feeding
+
+
+@pytest.mark.django_db
+def test_pony_stats():
+    my_pony = Pony.objects.create(name="Derpy Hooves")
+
+    assert 'Derpy' in str(my_pony)
