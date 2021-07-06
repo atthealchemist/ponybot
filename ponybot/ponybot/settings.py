92cb0f94eca195ed5b8b2d3699e3b2236d59cf64
The <at.thealchemist@gmail.com>
2021-07-06 09:48:35 +0300

    Fixing typo

--- ponybot/ponybot/settings.py
+++ ponybot/ponybot/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Third-party apps
-    'restframework',
+    'rest_framework',
     # User defined apps
     'pony'
 ]
