d6175e83b91ab3bf364e40ef03ff691ea5b6d4ff
The <at.thealchemist@gmail.com>
2021-07-10 08:14:17 +0300

    Added celery settings in project settings

--- ponybot/ponybot/settings.py
+++ ponybot/ponybot/settings.py
@@ -128,3 +128,6 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10
 }
+
+CELERY_BEAT_SCHEDULER = 'django_celery_beat.schedulers:DatabaseScheduler'
+CELERY_BROKER_URL = "pyamqp://guest@localhost//"
