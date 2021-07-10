a693a283b81b1acfed6f784bccde3e35a2a0dfb9
The <at.thealchemist@gmail.com>
2021-07-10 10:21:04 +0300

    Added bot service and notifier

---
+++ ponybot/bot/notifier.py
@@ -0,0 +1,31 @@
+from django.conf import settings
+
+from vk_api import VkApi
+from vk_api.bot_longpoll import VkBotLongPoll
+from vk_api.utils import get_random_id
+
+from abc import ABC as AbstractBase, abstractmethod
+
+
+class Notifier(AbstractBase):
+
+    @abstractmethod
+    def notify(self, sender, message):
+        pass
+
+
+class VkNotifier(Notifier):
+
+    def notify(self, sender, message):
+        self.api.messages.send(
+            peer_id=sender,
+            message=message,
+            random_id=get_random_id()
+        )
+
+    def __init__(self):
+        self.session = VkApi(
+            token=settings.VK_API_TOKEN,
+        )
+        self.long_poll = VkBotLongPoll(self.session, settings.VK_GROUP_ID)
+        self.api = self.session.get_api()
