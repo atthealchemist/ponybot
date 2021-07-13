ffdf921798da15836ee6babb0db8cfa410727613
The <at.thealchemist@gmail.com>
2021-07-13 08:42:07 +0300

    Added ponybot Action model

--- ponybot/bot/models.py
+++ ponybot/bot/models.py
@@ -1,7 +1,8 @@
 from django.contrib.auth.models import AbstractUser
 from django.utils.translation import gettext_lazy as _
 from django.db import models
-# Create your models here.
+
+from constance import config
 
 
 class PonybotUser(AbstractUser):
@@ -14,3 +15,17 @@ class PonybotUser(AbstractUser):
     class Meta:
         verbose_name = "Ponybot User"
         verbose_name_plural = "Ponybot Users"
+
+    def is_admin(self, user_id):
+        return self.is_superuser() or str(user_id) in config.PONY_BOT_ADMINS_LIST.split(',')
+
+
+class PonybotAction(models.Model):
+    name = models.CharField(_("Action Name"), max_length=64)
+    aliases = models.JSONField(_("Action aliases"), default=list)
+
+    is_admin_only = models.BooleanField(_("Is admin only"), default=False)
+
+    class Meta:
+        verbose_name = "Ponybot Action"
+        verbose_name_plural = "Ponybot Actions"
