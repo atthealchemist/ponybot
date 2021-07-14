bab3f1371c18fff413ac3d2f2162c996ad526aea
The <at.thealchemist@gmail.com>
2021-07-14 09:06:08 +0300

    Added DialogSession model admin

--- ponybot/bot/admin.py
+++ ponybot/bot/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 # Register your models here.
 from django.contrib import admin
 from django.contrib.admin.options import ModelAdmin
-from .models import PonybotAction, PonybotUser
+from .models import PonybotAction, PonybotUser, DialogSession
 
 
 @admin.register(PonybotUser)
@@ -20,3 +20,10 @@ class PonybotActionAdmin(ModelAdmin):
 
     def action_aliases(self, action):
         return ', '.join(action.aliases)
+
+
+@admin.register(DialogSession)
+class DialogSessionAdmin(ModelAdmin):
+    list_display = ('user_id', 'peer_id', 'action_id',
+                    'opened', 'last_message')
+    list_filter = ('opened',)
